df6c2fd614aec330cff9a8840400266aebb91dc4
Adapa Meenakshi <education213xz@gmail.com>
2025-07-29 12:09:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This forces the build to target a modern browser environment
+    // that supports import.meta.
+    target: 'esnext'
+  }
 })
+
